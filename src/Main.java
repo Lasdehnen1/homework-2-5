e9ce1f16baed0add93e3e7fa19407350740450fb
Evgenii.Pichugov <eugepic@yandex.ru>
2023-05-30 22:42:22 +0300

    2.4. Работа с исключениями

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String login = "boris1____";
+        String password = "112fdfsf____";
+        String confirmPassword = "112fdfsf____";
+
+
+        try {
+            check(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+        } catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
+    public static void check(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (login.length() > 20) {
+            throw new WrongLoginException("Превышена длина логина");
+        }
+        Pattern pt = Pattern.compile("^[aA-zZ(0-9)\\.]*$");
+        Matcher mt = pt.matcher(login);
+        if(!mt.matches()){
+            throw new WrongLoginException("Некорректный логин");
+        }
+        if (password.length() > 20) {
+            throw new WrongPasswordException("Превышена длина пароля");
+        }
+        Matcher mt1 = pt.matcher(password);
+        if(!mt1.matches()){
+            throw new WrongLoginException("Некорректный пароль");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
+    }
+
+
 }
